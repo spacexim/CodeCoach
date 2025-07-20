3d4b5fb294853fbc47750e104097e47a90577ee3
Yao Zu <1796381757@qq.com>
2025-07-20 18:43:16 +0800

    修复 Codespace 中的 Vite WebSocket 配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,9 +21,10 @@ const resolvedMonacoPlugin = isObjectWithDefault(monacoEditorPlugin)
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
+    port: 5173,
     hmr: {
-      protocol: "ws",
-      host: "localhost",
+      port: 5173,
     },
   },
   plugins: [react(), resolvedMonacoPlugin({})],
